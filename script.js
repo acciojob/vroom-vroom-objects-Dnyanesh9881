03fedaf9043e00c8fec0d0eb8539c4066f831be5
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-02-12 09:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Complete the js code
 function Car(make, model) {}
+Car.prototype.getMakeModel=()=> {
+	return make+" "+model;
+}
 
 function SportsCar(make, model, topSpeed) {}
-
+SportsCar.prototype.getTopSpeed()=()=> {
+	return topspeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
