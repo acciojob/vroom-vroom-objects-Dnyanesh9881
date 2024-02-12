ac6a33856f874005077dc5e3b054acd61570f5a4
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-02-12 09:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
-Car.prototype.getMakeModel=()=> {
-	return make+" "+model;
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 }
 
-function SportsCar(make, model, topSpeed) {}
-SportsCar.prototype.getTopSpeed=()=> {
-	return topspeed;
+// Adding a method to the Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+// Constructor function for SportsCar, inheriting from Car
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor using SportsCar's context
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
+
+// Inherit the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Set the constructor property back to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Adding a method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
